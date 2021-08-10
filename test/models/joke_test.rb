a3e69d1abe4b8570edc244e5fb998248a06310c2
ivanpiskun123 <ivan.piskun.s@gmail.com>
2021-08-10 14:51:05 +0300

    Full version

--- test/models/joke_test.rb
+++ test/models/joke_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class JokeTest < ActiveSupport::TestCase
-  test "should not save Joke without field Body" do
+  test "should not save joke without field body" do
         j = Joke.new
         assert_not j.save
   end
